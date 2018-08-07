afac2a3dbf84b098e454c6ee0faeba1ebd546a1c
Charles McGarvey <chazmcgarvey@brokenzipper.com>
2018-08-06 21:25:47 -0600

    Version 0.010

--- lib/Hash/Diff.pm
+++ lib/Hash/Diff.pm
@@ -8,7 +8,7 @@ use Hash::Merge;
 use base 'Exporter';
 use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS);
 
-$VERSION     = 0.009;
+$VERSION     = 0.010;
 @EXPORT_OK   = qw( diff left_diff );
 
 sub left_diff {
